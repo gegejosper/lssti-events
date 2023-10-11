a0310f33960056d4529ec2296b3acc9dfc0a03ad
Gegejosper Ceniza <gegejosper@gmail.com>
2023-10-11 10:37:27 +0800

    fix

--- app/Models/Att_punch.php
+++ app/Models/Att_punch.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Att_punch extends Model
 {
     use HasFactory;
+    protected $table = 'att_punches';
+    public $timestamps = false;
+
+    public function student(){
+        return $this->belongsTo('App\Models\Student', 'employee_id', 'id');
+    }
 }
